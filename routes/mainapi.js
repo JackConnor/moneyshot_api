8fb3be5394a7c868f8e20b88d9d8fe870f432cc4
Jack Connor <jack.connor83@gmail.com>
2016-04-12 17:58:27 -0700

    pushing new cors info

--- routes/mainapi.js
+++ routes/mainapi.js
@@ -54,8 +54,8 @@ module.exports = function(app){
 
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
-    res.header("Access-Control-Allow-Origin", "http://localhost:8101");
+    // res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+    // res.header("Access-Control-Allow-Origin", "http://localhost:8101");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
